736f2fe1f021ab0a42c56ecef32a64abaa08570f
SoeunPark <soeun1221@gmail.com>
2017-04-06 21:26:24 +0900

    프로젝트 관리 팀원 추가 기능 완성

--- src/com/lotte/emp/controller/EmpController.java
+++ src/com/lotte/emp/controller/EmpController.java
@@ -121,9 +121,6 @@ public class EmpController extends HttpServlet{
 		request.getRequestDispatcher(url).forward(request, response);
 	}
 	
-	//여기서부터 다ㅅ, 메뉴->목록출력->프로젝트관리->추가->insert->재정렬->>>>>
-	
-	
 	public void userList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String url = "view/error/listError.jsp"; // 에러 창으로 이동
 		try {
@@ -161,7 +158,7 @@ public class EmpController extends HttpServlet{
 			boolean check = service.insertTeamMember(eIndex, pIndex);
 			if(check == true){
 				url ="view/table/findEmpModal.jsp";
-				//request.setAttribute("check", check);
+				request.setAttribute("memList", service.memberList(pIndex));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
